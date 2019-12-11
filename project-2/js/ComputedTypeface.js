7b4d9a09241bebd4c227f114e19c2a4b983f3026
Sony Maharjan <smaharjan@cca.edu>
2019-12-10 16:36:13 -0800

    m

---
+++ project-2/js/ComputedTypeface.js
@@ -0,0 +1,12 @@
+
+
+<script>
+function myFunction() {
+  var x = document.getElementByID("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
